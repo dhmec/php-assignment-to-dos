4cd682f9b267398da3c5d4225a96c2c26c945254
dhmec <dharmoradiya@gmail.com>
2020-10-01 16:44:46 -0600

    add form with one input field, submit and reset button

--- index.php
+++ index.php
@@ -12,6 +12,28 @@
     <script type="text/JavaScript" src="js/scripts.js" defer></script>
 </head>
 <body>
+    <h1>Welcome to My To-Dos!</h1>
+    <h2>Add a To-Do</h2>
+    <?php
+    
+    echo '<pre>';
+    var_dump( $_GET );
+    var_dump( $_POST ); 
+    echo '</pre>';
+
+    ?>
+    <form method="GET" action="index.php">
+        <label for="task">
+            Enter a new task:
+            <input
+            id="task"
+            name="new-task"
+            type="text"
+            value="">
+        </label>
+        <input type="submit" value="Add To List">
+        <input type="reset" value="reset">
+    </form>
     
 </body>
 </html>
